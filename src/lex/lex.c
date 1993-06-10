da186d3d4a04369de1ad5a42135ecffe55a02ad8
Felix A. Croes <felix@dworkin.nl>
1993-06-10 11:44:49 +0000

    Initial revision

---
+++ src/lex/lex.c
@@ -0,0 +1,74 @@
+# include <ctype.h>
+# include "lex.h"
+# include "token.h"
+# include "ppcontrol.h"
+
+YYSTYPE yylval;
+
+char *paths[] = { "..", "/usr/include", 0 };
+
+void convert(s)
+register char *s;
+{
+    while (*s) {
+	*s = toupper(*s);
+	s++;
+    }
+}
+
+main(argc, argv)
+int argc;
+char *argv[];
+{
+    register int c;
+
+    pp_init(argv[1], paths);
+
+    while ((c=pp_gettok()) != EOF) {
+	switch (c) {
+	case STRING_CONST:
+	    printf(" \"%s\"", yylval.string);
+	    break;
+
+	case INT_CONST:
+	    printf(" %ld", (long) yylval.number);
+	    break;
+
+	case IDENTIFIER:
+	    printf(" %s", yylval.string);
+	    break;
+
+	default:
+	    if (c >= FOR && c <= SWITCH) {
+		convert(yytext);
+	    }
+	    printf(" %s", yytext);
+	    break;
+	}
+    }
+    putchar('\n');
+
+    pp_clear();
+    return 0;
+}
+
+void yyerror(s1, s2, s3)
+char *s1, *s2, *s3;
+{
+    fprintf(stderr, "\"%s\", line %u: ", tk_filename(), tk_line());
+    fprintf(stderr, s1, s2, s3);
+    fputc('\n', stderr);
+}
+
+void fatal(f, a1, a2)
+char *f, *a1, *a2;
+{
+    printf(f, a1, a2);
+    abort();
+}
+
+char *path_include(f, file)
+char *f, *file;
+{
+    return file;
+}
---
+++ src/lex/special.c
@@ -0,0 +1,56 @@
+# include "lex.h"
+# include "macro.h"
+# include "token.h"
+# include "special.h"
+
+/*
+ * Predefined macro handling.
+ */
+
+static char datestr[14];
+static char timestr[11];
+
+/*
+ * NAME:	special_define()
+ * DESCRIPTION:	predefine macros
+ */
+void special_define()
+{
+    extern long time P((long*));
+    extern char *ctime P((long*));
+    long t;
+    register char *ts;
+
+    mc_define("__LINE__", (char *) NULL, -1);
+    mc_define("__FILE__", (char *) NULL, -1);
+    mc_define("__DATE__", (char *) NULL, -1);
+    mc_define("__TIME__", (char *) NULL, -1);
+
+    t = time((long *) NULL);
+    ts = ctime(&t);
+    sprintf(datestr, "\"%.6s %.4s\"", ts + 4, ts + 20);
+    sprintf(timestr, "\"%.8s\"", ts + 11);
+}
+
+/*
+ * NAME:	special_replace()
+ * DESCRIPTION:	return the expandation of a predefined macro
+ */
+char *special_replace(name)
+register char *name;
+{
+    static char buf[STRINGSZ + 2];
+
+    if (strcmp(name, "__LINE__") == 0) {
+	sprintf(buf, " %u ", tk_line());
+	return buf;
+    } else if (strcmp(name, "__FILE__") == 0) {
+	sprintf(buf, "\"%s\"", tk_filename());
+	return buf;
+    } else if (strcmp(name, "__DATE__") == 0) {
+	return datestr;
+    } else if (strcmp(name, "__TIME__") == 0) {
+	return timestr;
+    }
+    return (char *) NULL;
+}
