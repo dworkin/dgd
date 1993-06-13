a9655384978c47eacf767d3ee96471f77a37f198
Felix A. Croes <felix@dworkin.nl>
1993-06-13 19:16:36 +0000

    Initial revision

---
+++ src/error.h
@@ -0,0 +1,11 @@
+# define ec_push()	setjmp(*_ec_push_())
+
+extern jmp_buf *_ec_push_	P((void));
+extern jmp_buf *ec_pop		P((void));
+
+extern char *errormesg		P((void));
+extern void  errorlog		P((FILE*));
+
+extern void  warning		();
+extern void  error		();
+extern void  fatal		();
