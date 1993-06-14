c6bde5164014a2a11eb772c2957999860a8a9a6c
Felix A. Croes <felix@dworkin.nl>
1993-06-14 17:12:03 +0000

    Initial revision

---
+++ src/str.h
@@ -0,0 +1,25 @@
+typedef struct _string_ {
+    unsigned short len;	/* string length */
+    uindex ref;		/* number of references + const bit */
+    union {
+	struct _string_ **strconst;	/* indirect reference to myself */
+	struct _strref_ *primary;	/* primary reference */
+    } u;
+    char text[1];	/* actual characters following this struct */
+} string;
+
+# define STR_CONST	((uindex) ((UINDEX_MAX + 1L) >> 1))
+# define STR_REF	(UINDEX_MAX - STR_CONST)
+
+extern void	str_init	P((void));
+extern string  *str_new		P((char*, long));
+# define str_ref(s)	((s)->ref++)
+extern void	str_del		P((string*));
+
+extern long	str_put		P((char*, long));
+extern void	str_clear	P((void));
+
+extern int	str_cmp		P((string*, string*));
+extern string  *str_add		P((string*, string*));
+extern int	str_index	P((string*, Int));
+extern string  *str_range	P((string*, Int, Int));
