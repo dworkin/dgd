297143a3ab915bdd339ae508b5cd8da3e7d587e7
Felix A. Croes <felix@dworkin.nl>
1993-06-15 20:40:49 +0000

    Initial revision

---
+++ src/comp/codegen.h
@@ -0,0 +1,2 @@
+extern char *cg_function	P((node*, int, int, unsigned short*));
+extern void  cg_clear		P((void));
---
+++ src/comp/control.h
@@ -0,0 +1,19 @@
+extern void	 ctrl_init	P((char*));
+extern void	 ctrl_inherit	P((object*, string*));
+extern void	 ctrl_create	P((void));
+extern long	 ctrl_dstring	P((string*));
+extern void	 ctrl_dproto	P((string*, char*));
+extern void	 ctrl_dfunc	P((string*, char*));
+extern void	 ctrl_dprogram	P((char*, unsigned short));
+extern void	 ctrl_dvar	P((string*, unsigned short, unsigned short));
+extern char	*ctrl_lfcall	P((string*, char*, long*));
+extern char	*ctrl_ifcall	P((string*, long*));
+extern char	*ctrl_fcall	P((string*, long*, bool));
+extern short	 ctrl_var	P((string*, long*));
+extern control	*ctrl_construct	P((char*));
+extern void	 ctrl_clear	P((void));
+
+# define KFCALL		0
+# define LFCALL		1
+# define DFCALL		2
+# define FCALL		3
