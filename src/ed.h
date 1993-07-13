712d95c65c27a6f4ef2ee2aeba8800b7af2d08cd
Felix A. Croes <felix@dworkin.nl>
1993-07-13 15:21:42 +0000

    1.0.a2

--- src/ed.h
+++ src/ed.h
@@ -1,2 +1,5 @@
-# include "line.h"
-# include "edcmd.h"
+extern void	ed_init		P((char*, int));
+extern void	ed_new		P((object*));
+extern void	ed_del		P((object*));
+extern void	ed_command	P((object*, char*));
+extern char    *ed_status	P((object*));
