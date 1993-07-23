5eebaa35bc10298ea71fd990933dc675fb18ed9c
Felix A. Croes <felix@dworkin.nl>
1993-07-23 22:57:56 +0000

    1.0.a2

--- src/path.h
+++ src/path.h
@@ -1,6 +1,7 @@
-char *path_resolve	P((char*));
-char *path_ed_read	P((char*));
-char *path_ed_write	P((char*));
-char *path_object	P((char*, char*));
-char *path_inherit	P((char*, char*));
-char *path_include	P((char*, char*));
+extern char *path_resolve	P((char*));
+extern char *path_file		P((char*));
+extern char *path_ed_read	P((char*));
+extern char *path_ed_write	P((char*));
+extern char *path_object	P((char*));
+extern char *path_inherit	P((char*, char*));
+extern char *path_include	P((char*, char*));
