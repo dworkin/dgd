f14b89def68e4fe8e915e55858404fe27b72bed1
Felix A. Croes <felix@dworkin.nl>
1993-07-26 14:00:19 +0000

    1.0.a2

--- src/host.h
+++ src/host.h
@@ -1,11 +1,20 @@
 # ifdef MINIX_68K
 
 # include <limits.h>
+# include <sys/types.h>
 
 # ifdef INCLUDE_FILE_IO
-# include <sys/types.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include <sys/stat.h>
+# endif
+
+# ifdef INCLUDE_TIME
+# include <time.h>
+# endif
+
+# ifdef INCLUDE_TELNET
+# include "host/telnet.h"
 # endif
 
 # include <stdlib.h>
@@ -26,17 +35,29 @@ typedef long Int;
 # define FS_BLOCK_SIZE		1024
 # define UNIX_PATH_RESOLVE
 
+/* simulated syscalls */
+extern int  rename	P((char*, char*));
+
 # endif
 
 
 # ifdef SUN
 
 # include <limits.h>
+# include <sys/types.h>
 
 # ifdef INCLUDE_FILE_IO
-# include <sys/types.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include <sys/stat.h>
+# endif
+
+# ifdef INCLUDE_TIME
+# include <time.h>
+# endif
+
+# ifdef INCLUDE_TELNET
+# include <arpa/telnet.h>
 # endif
 
 # include <alloca.h>
@@ -61,3 +82,17 @@ typedef int Int;
 
 
 # define ALIGN(x, s)	(((x) + (s) - 1) & ~((s) - 1))
+
+extern void  host_init		P((void));
+extern void  host_finish	P((void));
+extern void  host_message	P((char*));
+extern void  host_error		P((void));
+
+extern bool  _opendir		P((char*));
+extern char *_readdir		P((void));
+extern void  _closedir		P((void));
+
+extern void  _alarm		P((long));
+
+extern long  random		P((void));
+extern char *crypt		P((char*, char*));
