d755896a762894fbf1ff428d119345511d149d09
Felix A. Croes <felix@dworkin.nl>
1993-07-27 15:00:50 +0000

    1.0.a3

--- src/host/unix/rename.c
+++ src/host/unix/rename.c
@@ -8,14 +8,11 @@
 int rename(from, to)
 char *from, *to;
 {
-    static unsigned short count;
-    char buf[9];
-
-    /* check if 'from' can be linked at all */
-    sprintf(buf, "_rnm%04x", ++count);
-    if (strcmp(from, to) == 0 || link(from, buf) < 0 || unlink(buf) < 0 ||
-	(unlink(to) < 0 && errno != ENOENT) ||
-	link(from, to) < 0 || unlink(from) < 0) {
+    if (link(from, to) < 0) {
+	return -1;
+    }
+    if (unlink(from) < 0) {
+	unlink(to);
 	return -1;
     }
     return 0;
