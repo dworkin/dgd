be9beb2c6bed25905a51fd6e40d76ce759dfb7aa
Felix A. Croes <felix@dworkin.nl>
1993-08-08 00:28:04 +0000

    1.0.a3

--- src/object.h
+++ src/object.h
@@ -5,7 +5,8 @@ typedef struct _object_ {
     hte chain;			/* object name hash table */
     char flags;			/* object status */
     char eduser;		/* index in user/editor array */
-    objkey key;			/* object key */
+    uindex index;		/* index in object table */
+    Int count;			/* object creation count */
     union {
 	long ref;		/* ref count (if master object) */
 	struct _object_ *master;/* pointer to master object */
@@ -18,14 +19,15 @@ typedef struct _object_ {
 
 # define O_MASTER		0x01
 # define O_CREATED		0x02
-# define O_USER			0x04
-# define O_EDITOR		0x08
-# define O_DRIVER		0x10
+# define O_AUTO			0x04
+# define O_DRIVER		0x08
+# define O_USER			0x10
+# define O_EDITOR		0x20
 
 extern void		   o_init	P((int));
 extern object		  *o_new	P((char*, object*, struct _control_*));
 extern void		   o_del	P((object*));
-extern object		  *o_object	P((objkey*));
+extern object		  *o_object	P((uindex, Int));
 extern char		  *o_name	P((object*));
 extern void		   o_rename	P((object*, char*));
 extern object		  *o_find	P((char*));
@@ -33,5 +35,3 @@ extern struct _control_	  *o_control	P((object*));
 extern struct _dataspace_ *o_dataspace	P((object*));
 extern void		   o_clean	P((void));
 extern uindex		   o_count	P((void));
-
-# define DESTRUCTED(o)		(o_object(&(o)) == (object *) NULL)
