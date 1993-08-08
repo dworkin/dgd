1e2eccb402c5373ec33a96eca33ef8e6e2f915e8
Felix A. Croes <felix@dworkin.nl>
1993-08-08 00:52:39 +0000

    1.0.a3

--- src/comp/control.h
+++ src/comp/control.h
@@ -1,6 +1,6 @@
 extern void	 ctrl_init	P((char*));
 extern void	 ctrl_inherit	P((object*, string*));
-extern void	 ctrl_create	P((void));
+extern void	 ctrl_create	P((char*));
 extern long	 ctrl_dstring	P((string*));
 extern void	 ctrl_dproto	P((string*, char*));
 extern void	 ctrl_dfunc	P((string*, char*));
@@ -10,8 +10,10 @@ extern char	*ctrl_lfcall	P((string*, char*, long*));
 extern char	*ctrl_ifcall	P((string*, long*));
 extern char	*ctrl_fcall	P((string*, long*, bool));
 extern short	 ctrl_var	P((string*, long*));
-extern control	*ctrl_construct	P((char*));
+extern control	*ctrl_construct	P((void));
 extern void	 ctrl_clear	P((void));
+
+# define T_IMPLICIT	(T_VOID | (1 << REFSHIFT))
 
 # define KFCALL		0
 # define LFCALL		1
