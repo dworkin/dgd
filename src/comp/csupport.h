955302b90da63a7f751c18bf9e75e7e1959173e7
Felix A. Croes <felix@dworkin.nl>
1993-09-15 18:52:08 +0000

    1.0.a4

---
+++ src/comp/csupport.h
@@ -0,0 +1,48 @@
+typedef void (*pcfunc)();
+
+typedef struct {
+    char ninherits;		/* # of inherits */
+    char nvirtuals;		/* # of virtual inherits */
+    char **inherits;		/* inherits */
+
+    unsigned short progsize;	/* program size */
+    char *program;		/* program */
+
+    unsigned short nstrings;	/* # of strings */
+    char *stext;		/* string text */
+    unsigned short *slength;	/* string lengths */
+
+    unsigned short nfunctions;	/* # functions */
+    pcfunc *functions;		/* functions */
+
+    unsigned short nfuncdefs;	/* # function definitions */
+    dfuncdef *funcdefs;		/* function definitions */
+
+    unsigned short nvardefs;	/* # variable definitions */
+    dvardef *vardefs;		/* variable definitions */
+
+    uindex nfuncalls;		/* # function calls */
+    char *funcalls;		/* function calls */
+} precomp;
+
+extern precomp	*precompiled[];	/* table of precompiled objects */
+extern pcfunc	*pcfunctions;	/* table of precompiled functions */
+extern Int	 tv[];		/* tmpval table */
+
+
+void preload		P((void));
+
+# define PUSH_NUMBER	(--sp)->type = T_NUMBER, sp->u.number =
+# define push_lvalue(v)	((--sp)->type = T_LVALUE, sp->u.lval = (v))
+# define store()	(i_store(sp + 1, sp), sp[1] = sp[0], sp++)
+# define store_int()	(i_store(sp + 1, sp), sp += 2, sp[-2].u.number)
+# define truthval(v)	((v)->type != T_NUMBER || (v)->u.number != 0)
+
+void call_kfun		P((int));
+void call_kfun_arg	P((int, int));
+void check_int		P((value*));
+Int  xdiv		P((Int, Int));
+Int  xmod		P((Int, Int));
+bool poptruthval	P((void));
+int  switch_range	P((Int, Int*, int));
+int  switch_str		P((value*, char*, int));
