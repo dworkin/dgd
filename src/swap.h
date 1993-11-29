1255a0fdea681be49063d4e8959061af18152e79
Felix A. Croes <felix@dworkin.nl>
1993-11-29 14:04:02 +0000

    1.0.a6

--- src/swap.h
+++ src/swap.h
@@ -5,12 +5,15 @@ typedef uindex sector;
 
 # define SW_UNUSED	UINDEX_MAX
 
-extern void		sw_init	   P((char*, uindex, uindex, uindex));
-extern sector		sw_new	   P((void));
-extern void		sw_del	   P((sector));
-extern void		sw_readv   P((char*, sector*, long, long));
-extern void		sw_writev  P((char*, sector*, long, long));
-extern uindex		sw_mapsize P((long));
-extern uindex		sw_count   P((void));
+extern void	sw_init		P((char*, uindex, uindex, uindex));
+extern void	sw_finish	P((void));
+extern sector	sw_new		P((void));
+extern void	sw_del		P((sector));
+extern void	sw_readv	P((char*, sector*, long, long));
+extern void	sw_writev	P((char*, sector*, long, long));
+extern uindex	sw_mapsize	P((long));
+extern uindex	sw_count	P((void));
+extern bool	sw_dump		P((int));
+extern void	sw_restore	P((int));
 
 # endif	/* H_SWAP */
