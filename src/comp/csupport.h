dcde260722fef7617adb88b2d24c695300a6f022
Felix A. Croes <felix@dworkin.nl>
1993-12-13 15:59:13 +0000

    1.0.a6

--- src/comp/csupport.h
+++ src/comp/csupport.h
@@ -1,15 +1,21 @@
+typedef struct {
+    char *name;			/* name of object */
+    unsigned short funcoffset;	/* function call offset */
+    unsigned short varoffset;	/* variable offset */
+} pcinherit;
+
 typedef void (*pcfunc)();
 
 typedef struct {
     short ninherits;		/* # of inherits */
-    char **inherits;		/* inherits */
+    pcinherit *inherits;	/* inherits */
 
     unsigned short progsize;	/* program size */
     char *program;		/* program */
 
     unsigned short nstrings;	/* # of strings */
+    dstrconst* sstrings;	/* string constants */
     char *stext;		/* string text */
-    unsigned short *slength;	/* string lengths */
 
     unsigned short nfunctions;	/* # functions */
     pcfunc *functions;		/* functions */
@@ -22,13 +28,17 @@ typedef struct {
 
     uindex nfuncalls;		/* # function calls */
     char *funcalls;		/* function calls */
+
+    uindex nsymbols;		/* # symbols */
+    dsymbol *symbols;		/* symbols */
 } precomp;
 
 extern precomp	*precompiled[];	/* table of precompiled objects */
 extern pcfunc	*pcfunctions;	/* table of precompiled functions */
 
 
-void preload		P((void));
+void pc_preload		P((char*, char*));
+void pc_control		P((control*, object*));
 
 # define PUSH_NUMBER	(--sp)->type = T_NUMBER, sp->u.number =
 # define push_lvalue(v)	((--sp)->type = T_LVALUE, sp->u.lval = (v))
@@ -43,6 +53,6 @@ Int  xdiv		P((Int, Int));
 Int  xmod		P((Int, Int));
 bool poptruthval	P((void));
 void pre_catch		P((void));
-void post_catch		P((void));
+void post_catch		P((bool));
 int  switch_range	P((Int, Int*, int));
 int  switch_str		P((value*, char*, int));
