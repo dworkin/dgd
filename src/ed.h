287eff054e2b6ca574b145934e1f320a26ae85cd
Felix A. Croes <felix@dworkin.nl>
1994-04-25 16:53:39 +0000

    1.0.a7

--- src/ed.h
+++ src/ed.h
@@ -2,5 +2,5 @@ extern void	ed_init		P((char*, int));
 extern void	ed_finish	P((void));
 extern void	ed_new		P((object*));
 extern void	ed_del		P((object*));
-extern void	ed_command	P((object*, char*));
+extern string  *ed_command	P((object*, char*));
 extern char    *ed_status	P((object*));
