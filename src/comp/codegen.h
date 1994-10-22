97ecf07d33e03036b8c986f516fce983f2f948f5
Felix A. Croes <felix@dworkin.nl>
1994-10-22 15:50:58 +0000

    1.0.9

--- src/comp/codegen.h
+++ src/comp/codegen.h
@@ -1,6 +1,6 @@
-extern void  cg_init		P((bool));
+extern void  cg_init		P((int));
 extern bool  cg_compiled	P((void));
-extern char *cg_function	P((string*, node*, int, int, unsigned short,
+extern char *cg_function	P((string*, node*, int, int, unsigned int,
 				   unsigned short*));
 extern int   cg_nfuncs		P((void));
 extern void  cg_clear		P((void));
