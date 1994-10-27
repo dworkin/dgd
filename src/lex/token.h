4cc0ab8b666c10ddd60c647c09a6cf81527ba475
Felix A. Croes <felix@dworkin.nl>
1994-10-27 16:00:26 +0000

    1.0.9

--- src/lex/token.h
+++ src/lex/token.h
@@ -4,12 +4,12 @@ extern bool		 tk_include	P((char*));
 extern void		 tk_endinclude	P((void));
 extern unsigned short	 tk_line	P((void));
 extern char		*tk_filename	P((void));
-extern void		 tk_setline	P((unsigned short));
+extern void		 tk_setline	P((unsigned int));
 extern void		 tk_setfilename	P((char*));
-extern void		 tk_header	P((bool));
-extern void		 tk_setpp	P((bool));
+extern void		 tk_header	P((int));
+extern void		 tk_setpp	P((int));
 extern int		 tk_gettok	P((void));
-extern void		 tk_skiptonl	P((bool));
+extern void		 tk_skiptonl	P((int));
 extern int		 tk_expand	P((macro*));
 
 extern char *yytext;
