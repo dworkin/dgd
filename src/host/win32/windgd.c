a9f42b363c41a9efae5187facbce574a1c08b0dc
Bart van Leeuwen <aidil@wotf.org>
2010-03-18 20:37:21 +0000

    Also add the new files for the windows build to the trunk.. :)

---
+++ src/host/win32/windgd.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of DGD, http://dgd-osr.sourceforge.net/
+ * Copyright (C) 1993-2010 Dworkin B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "dgd.h"
+
+int main(int argc, char **argv)
+{
+    return dgd_main(argc, argv);
+}
+
+/*
+ * NAME:    P->message()
+ * DESCRIPTION: show message
+ */
+void P_message(char *mess)
+{
+    fputs(mess, stdout);
+    fflush(stdout);
+}
