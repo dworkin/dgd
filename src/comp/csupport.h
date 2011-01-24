33be8019624d0bae495ed2e51f68ef2743425b4c
Felix A. Croes <felix@dworkin.nl>
2011-01-24 14:57:22 +0100

    Minimize comp/csupport.c and comp/csupport.h.

--- src/comp/csupport.h
+++ src/comp/csupport.h
@@ -16,61 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-typedef struct {
-    char *name;			/* name of object */
-    uindex progoffset;		/* program offset */
-    uindex funcoffset;		/* function call offset */
-    unsigned short varoffset;	/* variable offset */
-    bool priv;			/* privately inherited? */
-} pcinherit;
-
 typedef void (*pcfunc) P((frame*));
 
-typedef struct {
-    uindex oindex;		/* precompiled object */
-
-    short ninherits;		/* # of inherits */
-    pcinherit *inherits;	/* inherits */
-
-    uindex imapsz;		/* inherit map size */
-    char *imap;			/* inherit map */
-
-    Uint compiled;		/* compile time */
-
-    unsigned short progsize;	/* program size */
-    char *program;		/* program */
-
-    unsigned short nstrings;	/* # of strings */
-    dstrconst* sstrings;	/* string constants */
-    char *stext;		/* string text */
-    Uint stringsz;		/* string size */
-
-    unsigned short nfunctions;	/* # functions */
-    pcfunc *functions;		/* functions */
-
-    short nfuncdefs;		/* # function definitions */
-    dfuncdef *funcdefs;		/* function definitions */
-
-    short nvardefs;		/* # variable definitions */
-    short nclassvars;		/* # class variable definitions */
-    dvardef *vardefs;		/* variable definitions */
-    char *classvars;		/* variable classes */
-
-    uindex nfuncalls;		/* # function calls */
-    char *funcalls;		/* function calls */
-
-    uindex nsymbols;		/* # symbols */
-    dsymbol *symbols;		/* symbols */
-
-    unsigned short nvariables;	/* # variables */
-    char *vtypes;		/* variable types */
-
-    short typechecking;		/* typechecking level */
-} precomp;
-
-extern precomp	*precompiled[];	/* table of precompiled objects */
 extern pcfunc	*pcfunctions;	/* table of precompiled functions */
-
 
 bool   pc_preload	P((char*, char*));
 array *pc_list		P((dataspace*));
