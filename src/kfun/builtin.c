098b2b8271f5f4f81fbc7311fb9aea55e4c993e4
Felix A. Croes <felix@dworkin.nl>
2011-05-13 15:51:25 +0200

    Ensure that buffers for int=>str conversion are large enough.
    Signed-off-by: Felix A. Croes <felix@dworkin.nl>

--- src/kfun/builtin.c
+++ src/kfun/builtin.c
@@ -2425,7 +2425,7 @@ char pt_add_int_string[] = { C_STATIC, 2, 0, 0, 8, T_STRING, T_INT, T_STRING };
  */
 int kf_add_int_string(frame *f)
 {
-    char buffer[10], *num;
+    char buffer[12], *num;
     string *str;
     long l;
 
@@ -2507,7 +2507,7 @@ char pt_add_string_int[] = { C_STATIC, 2, 0, 0, 8, T_STRING, T_STRING, T_INT };
  */
 int kf_add_string_int(frame *f)
 {
-    char buffer[10], *num;
+    char buffer[12], *num;
     string *str;
 
     i_add_ticks(f, 2);
