2df5266437c5c0b3958f52259398472ffd38f250
Felix A. Croes <felix@dworkin.nl>
2011-07-17 22:14:42 +0200

    Implement the relay program.
    Relay is a small program that connects to a DGD server, logs in as
    directed by an input script, and then executes a secondary program
    with the connection to the server as stdin and stdout.
    
    Relay is intended for those cases where integrating foreign code into
    DGD is hard, because of DGD's deliberate limitations, because of atomic
    rollback, or because DGD's internal structure is a bad match for what is
    required.

--- src/relay/login.c
+++ src/relay/login.c
@@ -15,3 +15,130 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+# include <sys/time.h> 
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <errno.h>
+# include "dgd.h"
+
+
+/*
+ * NAME:	login_dgd()
+ * DESCRIPTION:	connect to DGD server, login, and return a socket
+ */
+int login_dgd(char *hostname, int port, int in, int out)
+{
+    char input[10240], output[10240];
+    struct addrinfo hint, *res;
+    struct sockaddr_in6 sin;
+    int sock, i, n;
+    fd_set fds;
+
+    /*
+     * get server address
+     */
+    memset(&hint, '\0', sizeof(struct addrinfo));
+    hint.ai_family = PF_INET6;
+    if (getaddrinfo(hostname, NULL, &hint, &res) != 0) {
+	memset(&hint, '\0', sizeof(struct addrinfo));
+	hint.ai_family = PF_INET;
+	if (getaddrinfo(hostname, NULL, &hint, &res) != 0) {
+	    fprintf(stderr, "unknown host: %s\n", hostname);
+	    return -1;
+	}
+    }
+    memcpy(&sin, res->ai_addr, res->ai_addrlen);
+    freeaddrinfo(res);
+    sin.sin6_port = htons(port);
+
+    /*
+     * establish connection
+     */
+    sock = socket(sin.sin6_family, SOCK_STREAM, 0);
+    if (sock < 0) {
+	perror("socket");
+	return -1;
+    }
+    if (connect(sock, (struct sockaddr *) &sin,
+		(sin.sin6_family == AF_INET6) ?
+		 sizeof(struct sockaddr_in6) : sizeof(struct sockaddr_in)) < 0)
+    {
+	perror("connect");
+	close(sock);
+	return -1;
+    }
+
+    sleep(1);	/* give server time to send banner */
+    memset(&fds, '\0', sizeof(fd_set));
+    i = 0;
+
+    for (;;) {
+	/*
+	 * wait for input
+	 */
+	FD_SET(in, &fds);
+	FD_SET(sock, &fds);
+	if (select(2, &fds, NULL, NULL, NULL) <= 0) {
+	    perror("select");
+	    close(sock);
+	    return -1;
+	}
+
+	if (FD_ISSET(in, &fds)) {
+	    /*
+	     * input from stdin: read one character
+	     */
+	    n = read(in, input + i, 1);
+	    if (n < 0) {
+		perror("read");
+		close(sock);
+		return -1;
+	    }
+	    if (n == 0) {
+		/*
+		 * no more input; flush the input buffer if needed, and
+		 * return the socket
+		 */
+		if (i != 0 && write(sock, input, i) != i) {
+		    perror("socket write");
+		    close(sock);
+		    return -1;
+		}
+		return sock;
+	    }
+	}
+
+	if (FD_ISSET(sock, &fds)) {
+	    /*
+	     * input from socket: copy directly to stdout
+	     */
+	    n = read(sock, output, sizeof(output));
+	    if (n <= 0) {
+		perror("socket read");
+		close(sock);
+		return -1;
+	    }
+	    if (write(out, output, n) != n) {
+		perror("write");
+		close(sock);
+		return -1;
+	    }
+	}
+
+	if (FD_ISSET(in, &fds) && (input[i++] == '\n' || i == sizeof(input))) {
+	    /*
+	     * forward a line of input to the server
+	     */
+	    if (write(sock, input, i) != i) {
+		perror("socket write");
+		close(sock);
+		return -1;
+	    }
+	    i = 0;
+	    sleep(1);	/* give server time to respond */
+	}
+    }
+}
--- src/relay/relay.c
+++ src/relay/relay.c
@@ -15,3 +15,57 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+# include "dgd.h"
+# include "login.h"
+
+/*
+ * NAME:	main()
+ * DESCRIPTION:	Relay can be run as follows:
+ *
+ *		    relay hostname port program+arguments < input > /dev/null
+ *
+ *		which would connect to the server at hostname:port, read lines
+ *		from the file input and forward them to the server, and read
+ *		and discard any output from the server received before the
+ *		last line of input was read.
+ *		Once all lines from the input file are copied to the server,
+ *		the given program will be executed with the socket as stdin
+ *		and stdout.
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+    int port, socket;
+
+    /*
+     * check arguments
+     */
+    if (argc < 4) {
+	fprintf(stderr, "Usage: %s hostname port program [arguments...]\n",
+		argv[0]);
+	return 2;
+    }
+    port = atoi(argv[2]);
+    if (port <= 0 || port > 65535) {
+	fprintf(stderr, "%s: bad port\n", argv[0]);
+	return 2;
+    }
+
+    /*
+     * login on server
+     */
+    socket = login_dgd(argv[1], port, 0, 1);
+    if (socket < 0) {
+	return 1;
+    }
+
+    /*
+     * execute program
+     */
+    dup2(socket, 0);
+    dup2(socket, 1);
+    close(socket);
+    execvp(argv[3], argv + 3);
+    perror("execvp");
+    return 1;
+}
