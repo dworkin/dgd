46626da490b0a9d0963ebbdf9a9df79ad256deae
Felix A. Croes <felix@dworkin.nl>
2012-01-26 20:54:15 +0800

    1.4.13

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, http://dgd-osr.sourceforge.net/
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2011 DGD Authors (see the file Changelog for details)
+ * Copyright (C) 2010-2012 DGD Authors (see the file Changelog for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,4 +17,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-# define VERSION	"DGD 1.4.12"
+# define VERSION	"DGD 1.4.13"
