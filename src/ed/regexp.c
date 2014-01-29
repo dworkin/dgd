0eba00b2a6fcc3f6a741f1211298173c20f67f7c
Felix A. Croes <felix@dworkin.nl>
2014-01-29 08:20:42 +0800

    Fix inversion of character class.

--- src/ed/regexp.c
+++ src/ed/regexp.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010,2014 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -187,8 +187,8 @@ char *rx_comp(rxbuf *rx, char *pattern)
 		/* invert the whole cclass */
 		i = CCLSIZE;
 		do {
-		    cclass[i] ^= 0xff;
-		} while (--i > 0);
+		    cclass[--i] ^= 0xff;
+		} while (i > 0);
 		*cclass &= ~1;	/* never matches 0 */
 	    }
 
