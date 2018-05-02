48b34db0edaffdbf252f9fb7255bd04ff1e8de9e
Felix A. Croes <felix@dworkin.nl>
2018-05-02 17:37:15 +0200

    Avoid the direct use of malloc.

--- src/data.cpp
+++ src/data.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2016 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2018 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -46,7 +46,7 @@ struct copatch {
 
 # define COPCHUNKSZ	32
 
-class coptable {
+class coptable : public Allocated {
 public:
     /*
      * NAME:		coptable()
