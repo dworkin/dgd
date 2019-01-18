538e8701a4dd111c6ca2c07c8fecda52a7fd0065
Felix A. Croes <felix@dworkin.nl>
2019-01-18 22:47:14 +0100

    Try to detect use-after-free.

--- src/alloc.cpp
+++ src/alloc.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2018 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2019 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -275,6 +275,9 @@ public:
      * free static memory
      */
     static void free(MemChunk *c) {
+# ifdef DEBUG
+	memset(c + 1, '\xdd', c->size - sizeof(MemChunk));
+# endif
 	if (c->size < SLIMIT) {
 	    /* small chunk */
 	    c->next = schunks[(c->size - MOFFSET) / STRUCT_AL - 1];
@@ -706,6 +709,9 @@ public:
 	    return;
 	}
 
+# ifdef DEBUG
+	memset(c + 1, '\xdd', c->size - sizeof(MemChunk) - SIZETSIZE);
+# endif
 	if (c->size < DLIMIT) {
 	    /* small chunk */
 	    c->next = dchunks[(c->size - MOFFSET) / STRUCT_AL - 1];
--- src/alloc.h
+++ src/alloc.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2018 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2019 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -238,6 +238,9 @@ private:
 	Titem *item;
 
 	item = (Titem *) ptr;
+# ifdef DEBUG
+	memset(item, '\xdd', sizeof(Titem));
+# endif
 	item->list = flist;
 	flist = item;
     }
