e5bff252f2bfcebf481bdf266fa04933da43c7e8
Felix A. Croes <felix@dworkin.nl>
2021-02-10 14:18:19 +0100

    1.6.15

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2020 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2021 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,4 +17,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-# define VERSION	"DGD 1.6.14"
+# define VERSION	"DGD 1.6.15"
