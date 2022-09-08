8d586d45be2016e81e4338f4d73b81eec1ad58c9
Felix A. Croes <felix@dworkin.nl>
2022-09-08 23:47:45 +0200

    Add 64 bit support for random().

--- src/kfun/extra.cpp
+++ src/kfun/extra.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2021 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2022 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -390,8 +390,18 @@ int kf_random(Frame *f, int n, KFun *kf)
     if (f->sp->number < 0) {
 	return 1;
     }
-    PUT_INT(f->sp, (f->sp->number > 0) ?
-		    P_random() % f->sp->number : P_random());
+    if (f->sp->number == 0) {
+# ifdef LARGENUM
+	PUT_INT(f->sp, ((LPCuint) (Uint) P_random() << 31) ^ (Uint) P_random());
+    } else if (f->sp->number > 0xffffffffL) {
+	PUT_INT(f->sp, (((LPCuint) (Uint) P_random() << 31) ^
+			(Uint) P_random()) & f->sp->number);
+# else
+	PUT_INT(f->sp, (Uint) P_random() & 0x7fffffffL);
+# endif
+    } else {
+	PUT_INT(f->sp, (Uint) P_random() % f->sp->number);
+    }
     return 0;
 }
 # endif
