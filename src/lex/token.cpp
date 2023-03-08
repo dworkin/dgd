a3ed90ee3e043d07f026363eed689b881a513c9b
Felix A. Croes <felix@dworkin.nl>
2023-03-08 21:50:22 +0100

    Allow: int /* \n */ #line 100 \n a;

--- src/lex/token.cpp
+++ src/lex/token.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2022 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2023 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -340,6 +340,9 @@ void TokenBuf::skip_comment()
 	    if (c == EOF) {
 		error("EOF in comment");
 		return;
+	    }
+	    if (c == LF) {
+		seen_nl = TRUE;
 	    }
 	} while (c != '*');
 
