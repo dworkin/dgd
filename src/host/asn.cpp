207691e69764f8876b090a76f4a77ec6ec65fabe
Felix A. Croes <felix@dworkin.nl>
2023-09-20 21:18:29 +0200

    Fix leading \xff bytes for negative ASNs.

--- src/host/asn.cpp
+++ src/host/asn.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of DGD, https://github.com/dworkin/dgd
  * Copyright (C) 1993-2010 Dworkin B.V.
- * Copyright (C) 2010-2022 DGD Authors (see the commit log for details)
+ * Copyright (C) 2010-2023 DGD Authors (see the commit log for details)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -1210,9 +1210,7 @@ String *Asi::numtostr(bool minus)
 	} else {
 	    /* skip leading 0xff bytes */
 	    for (len = 24; UCHAR(bits >> len) == 0xff; len -= 8) ;
-	    if (!((bits >> len) & 0x80)) {
-		prefix = TRUE;
-	    }
+	    prefix = TRUE;
 	}
     } else {
 	/* skip leading 0x00 bytes */
